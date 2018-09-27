7925f5ab50c443a93bc6224aac909314e9d9f623
Ryan Mario <ryanmario87@gmail.com>
2018-09-27 16:54:54 +0800

    First Commit

---
+++ src/main/java/com/mitrais/carrot/config/error/ErrorResponseBody.java
@@ -0,0 +1,43 @@
+package com.mitrais.carrot.config.error;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * error custom response body
+ * 
+ * @author Febri
+ */
+@Data
+public class ErrorResponseBody {
+
+	/**
+	 * timestamp variable as Date type
+	 */
+	private Date timestamp;
+
+	/**
+	 * String details of errors
+	 */
+	private String details;
+
+	/**
+	 * String message to descripe the error
+	 */
+	private String message;
+
+	/**
+	 * ErrorResponseBody constructor
+	 * 
+	 * @param timestamp the date that error happen
+	 * @param message the message to describe the error
+	 * @param details the details of errors
+	 */
+	public ErrorResponseBody(Date timestamp, String message, String details) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+}
